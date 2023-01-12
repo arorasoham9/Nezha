ca8bf07b24be1334b04f41ea28cab77383ff5947
Mark <mbaumste@purdue.edu>
2023-01-12 13:02:36 -0500

    auto generated login and landing page components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,

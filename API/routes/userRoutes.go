216df5aaa4a23c5892be09e234f8b7a876f04355
Mark <mbaumste@purdue.edu>
2023-02-02 15:28:22 -0500

    Intial user routes

---
+++ API/routes/userRoutes.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"github.com/arorasoham9/ECE49595_PROJECT/API/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+}

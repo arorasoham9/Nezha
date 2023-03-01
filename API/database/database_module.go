b3ff3183793aeed67c903636951012891df5a585
Mark <mbaumste@purdue.edu>
2023-03-01 15:10:58 -0500

    Added go doc comments

--- API/database/database_module.go
+++ API/database/database_module.go
@@ -1,3 +1,4 @@
+// Package database
 package database
 
 import (
@@ -10,10 +11,12 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+// DatabaseModule represents a struct that holds a mongoClient allowing for Collection and Database access with extra error handling.
 type DatabaseModule struct {
 	client *mongo.Client
 }
 
+// getClient returns a Mongo Client from the Database instance
 func (d DatabaseModule) getClient() *mongo.Client {
 	if d.client == nil {
 		d.client = DBinstance()
@@ -21,12 +24,15 @@ func (d DatabaseModule) getClient() *mongo.Client {
 	return d.client
 }
 
+// openCollection returns a Mongo Collection from a specific database with name collectionName
 func (d DatabaseModule) openCollection(collectionName string) *mongo.Collection {
 	db := d.openDatabse("test")
 	var collection *mongo.Collection = db.Collection(collectionName)
 	return collection
 }
 
+// GetEmailCount returns int64,error, the int represents the count of a certain email, and error if there was an error counting documents
+// This function may become deprecated soon, was mainly used for testing and setting up API code.
 func (d DatabaseModule) GetEmailCount(collectionName string, email string) (int64, error) {
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 	userCollection := d.openCollection(collectionName)
@@ -39,6 +45,9 @@ func (d DatabaseModule) GetEmailCount(collectionName string, email string) (int6
 	return count, err
 }
 
+// FindUserByEmail returns a Models User and error.
+// The models User represents the user if they were fond
+// Erorr represents any errors encountered
 func (d DatabaseModule) FindUserByEmail(collectionName string, email string) (models.User, error) {
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 	userCollection := d.openCollection(collectionName)
@@ -49,16 +58,20 @@ func (d DatabaseModule) FindUserByEmail(collectionName string, email string) (mo
 	return foundUser, err
 }
 
+// AddUser
 func (d DatabaseModule) AddUser(email string) {
 	return
 }
 
+// openDatabase returns a Mongo Database
+// The Database of name dbname is opened using the client specified in the struct.
 func (d DatabaseModule) openDatabse(dbname string) *mongo.Database {
 	cl := d.getClient()
 	db := cl.Database(dbname)
 	return db
 }
 
+// CreateCollection
 func (d DatabaseModule) CreateCollection(collectionName string) {
 	return
 }
